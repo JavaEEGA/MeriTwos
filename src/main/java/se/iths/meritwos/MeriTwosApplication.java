256320585dbbfacb0a393df92592e079ddb4918f
Oliver Säfström <safstrom.oliver@gmail.com>
2023-03-09 09:33:31 +0100

    Initial commit

---
+++ src/main/java/se/iths/meritwos/MeriTwosApplication.java
@@ -0,0 +1,13 @@
+package se.iths.meritwos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeriTwosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeriTwosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/meritwos/MeriTwosApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.meritwos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class MeriTwosApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+
+//}
