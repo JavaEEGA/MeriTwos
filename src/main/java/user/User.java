6bafdb2263455825eecb42b98f28b79c6d9559d0
Oliver Säfström <safstrom.oliver@gmail.com>
2023-03-16 11:12:00 +0100

    Add User

---
+++ src/main/java/user/User.java
@@ -0,0 +1,43 @@
+package user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String password;
+
+    private Role role;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
+    enum Role {
+        ADMIN, STUDENT, COMPANY
+    }
+}
