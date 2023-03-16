0e6c117ff7f0992ab3e59a667188f0674be20861
Oliver Säfström <safstrom.oliver@gmail.com>
2023-03-16 14:37:53 +0100

    Update UserController, Add ResponeBodyMatchers

--- src/test/java/se/iths/meritwos/user/UserControllerTest.java
+++ src/test/java/se/iths/meritwos/user/UserControllerTest.java
@@ -7,10 +7,17 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import se.iths.meritwos.ResponseBodyMatchers;
 import se.iths.meritwos.mapper.Mapper;
 import se.iths.meritwos.user.UserController;
 import se.iths.meritwos.user.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @WebMvcTest(controllers = UserController.class)
 class UserControllerTest {
@@ -23,11 +30,16 @@ class UserControllerTest {
     @MockBean
     Mapper mapper;
 
+
     @Test
-    void getAllUserShouldReturnListOfUsers() {
+    void getAllUserShouldReturnOK() throws Exception {
         var user = new User(1L, "Oliver", "12345", User.Role.ADMIN);
         var user2 = new User(2L, "William", "2345", User.Role.STUDENT);
+        var userList = List.of(user, user2);
 
+        when(repository.findAll()).thenReturn(userList);
 
+        mockMvc.perform(get("/users")).andExpect(status().isOk());
     }
+
 }
