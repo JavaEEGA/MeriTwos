05496d7c95bdacd488e79c7415572bffe2c0aac6
Oliver Säfström <safstrom.oliver@gmail.com>
2023-03-16 14:37:53 +0100

    Update UserController, Add ResponeBodyMatchers

--- src/main/java/se/iths/meritwos/user/UserController.java
+++ src/main/java/se/iths/meritwos/user/UserController.java
@@ -1,11 +1,13 @@
 package se.iths.meritwos.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.iths.meritwos.mapper.Mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -20,9 +22,14 @@ public class UserController {
         this.mapper = mapper;
     }
 
-//    @GetMapping
-//    List<UserDTO> getAllUsers(){
-//
-//    }
+    @GetMapping
+    List<UserDTO> getAllUsers() {
+        return mapper.mapUserToDTO(userRepository.findAll());
+    }
 
+    @GetMapping("/{id}")
+    Optional<UserDTO> getUserByID(@PathVariable long id) {
+        return mapper.mapUserToDTO(userRepository.findById(id).get());
+
+    }
 }
