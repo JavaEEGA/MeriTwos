ffa934b7dac79be852682c0a46f565a1887e4770
Signe <signe.alfonson@hotmail.com>
2023-03-17 09:59:35 +0100

    update AdDTO and AdController with mapper
    Co-authored-by: william.karlsson1999@gmail.com

--- src/main/java/se/iths/meritwos/mapper/Mapper.java
+++ src/main/java/se/iths/meritwos/mapper/Mapper.java
@@ -1,6 +1,8 @@
 package se.iths.meritwos.mapper;
 
 import org.springframework.stereotype.Component;
+import se.iths.meritwos.ad.Ad;
+import se.iths.meritwos.ad.AdDTO;
 import se.iths.meritwos.user.User;
 import se.iths.meritwos.user.UserDTO;
 
@@ -19,4 +21,7 @@ public class Mapper {
         return Optional.of(new UserDTO(user));
     }
 
+    public List<AdDTO> mapAdToDTO(List<Ad> ads) {return ads.stream().map(AdDTO::new).toList();}
+
+    public Optional<AdDTO> mapAdToDTO(Ad ad) {return Optional.of((new AdDTO(ad)));}
 }
