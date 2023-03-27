1442691271a849cdb83ec6d8576f657b963a0800
William Karlsson <william.karlsson1999@gmail.com>
2023-03-27 10:02:07 +0200

    fix

--- src/main/java/se/iths/meritwos/webcontroller/AdWebController.java
+++ src/main/java/se/iths/meritwos/webcontroller/AdWebController.java
@@ -1,17 +1,14 @@
 package se.iths.meritwos.webcontroller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import se.iths.meritwos.ad.AdRepository;
 import org.springframework.stereotype.Controller;
 import se.iths.meritwos.company.CompanyRepository;
 
 @Controller
 public class AdWebController {
-    AdRepository adRepository;
     CompanyRepository companyRepository;
 
-    public AdWebController(AdRepository adRepository, CompanyRepository companyRepository){
-        this.adRepository = adRepository;
+    public AdWebController(CompanyRepository companyRepository){
         this.companyRepository = companyRepository;
     }
     @GetMapping (path = "/ads")
