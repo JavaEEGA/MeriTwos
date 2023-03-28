6b7a70b354d62d67299f3abfe5b9dc568b4b9dec
Oliver Säfström <safstrom.oliver@gmail.com>
2023-03-28 15:43:47 +0200

    Update loadUserByName to return Optional

--- src/main/java/se/iths/meritwos/security/DatabaseUserDetailService.java
+++ src/main/java/se/iths/meritwos/security/DatabaseUserDetailService.java
@@ -7,12 +7,10 @@ import org.springframework.stereotype.Service;
 import se.iths.meritwos.user.User;
 import se.iths.meritwos.user.UserRepository;
 
-import java.util.Set;
-
 @Service
 public class DatabaseUserDetailService implements UserDetailsService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     private DatabaseUserDetailService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -21,10 +19,7 @@ public class DatabaseUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        //TODO
-        User user = userRepository.findByName(userName);
-        if (user == null)
-            throw new UsernameNotFoundException("User not found");
+        User user = userRepository.findByName(userName).orElseThrow();
 
 
         return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), user.getRole());
